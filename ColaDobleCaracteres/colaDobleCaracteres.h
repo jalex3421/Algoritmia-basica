aa48d9fd4e7d93aa33d682742b48c8b3927fca83
Alejandro Meza <56438011+jalex3421@users.noreply.github.com>
2020-04-15 15:07:43 +0200

    Create colaDobleCaracteres.h

---
+++ ColaDobleCaracteres/colaDobleCaracteres.h
@@ -0,0 +1,30 @@
+
+
+#include <stdbool.h>
+
+ typedef char tipoElementoColaDoble;
+ typedef struct celdaCD{
+	 tipoElementoColaDoble elem;
+	 struct celdaCD *ant;
+	 struct celdaCD *sig;
+ } celdaColaDoble; 
+typedef struct tipoCD{
+	celdaColaDoble* ini;
+	celdaColaDoble* fin;
+}tipoColaDoble;
+
+void nuevaColaDoble(tipoColaDoble *); //
+
+void encolarPrimero(tipoColaDoble *, tipoElementoColaDoble);
+
+void encolarUltimo(tipoColaDoble *, tipoElementoColaDoble);
+
+void desencolarPrimero(tipoColaDoble *);
+
+void desencolarUltimo(tipoColaDoble *);
+
+tipoElementoColaDoble elemPrimero(tipoColaDoble); //
+
+tipoElementoColaDoble elemUltimo(tipoColaDoble); //
+
+bool esNulaColaDoble(tipoColaDoble); //
